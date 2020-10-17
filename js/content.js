2e160ea601723db265ba31af9b7631757c463b19
Everkers <waelkourich12@gmail.com>
2020-10-17 16:56:50 +0100

    init

--- js/content.js
+++ js/content.js
@@ -1,29 +1,32 @@
-if (typeof(EXT_NAME_CONTENT_SCRIPT_LOADED) == 'undefined') {
-	var EXT_NAME_CONTENT_SCRIPT_LOADED = true;
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+/* eslint-disable block-scoped-var */
+/* eslint-disable max-len */
+if (typeof (EXT_NAME_CONTENT_SCRIPT_LOADED) === 'undefined') {
+  // eslint-disable-next-line no-var
+  var EXT_NAME_CONTENT_SCRIPT_LOADED = true;
 
-	var ExtName = {};
+  const ExtName = {};
 
-	//---------------------------------------------------------------------------------------------------------------------
-	ExtName.initialize = function() {
-		console.log("Initializing extension content script");
+  //---------------------------------------------------------------------------------------------------------------------
+  ExtName.initialize = function () {
+    console.log('Initializing extension content script');
 
-		// On document ready
-		$(document).ready(function() {
+    // On document ready
+    $(document).ready(() => {
+      // Load CSS
+      const path = chrome.extension.getURL('css/content.css');
+      const link = document.createElement('link');
+      link.setAttribute('rel', 'stylesheet');
+      link.setAttribute('type', 'text/css');
+      link.setAttribute('href', path);
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }); // End of document.ready
 
-			// Load CSS
-			var path = chrome.extension.getURL('css/content.css');
-			var link = document.createElement('link')
-			link.setAttribute('rel', 'stylesheet')
-			link.setAttribute('type', 'text/css')
-			link.setAttribute('href', path)
-			document.getElementsByTagName('head')[0].appendChild(link)
+    // End of initialize
+  };
 
-		}); // End of document.ready
-
-		// End of initialize
-	}
-
-	//---------------------------------------------------------------------------------------------------------------------
-	// Start the extension content script
-	ExtName.initialize();
+  //---------------------------------------------------------------------------------------------------------------------
+  // Start the extension content script
+  ExtName.initialize();
 }
